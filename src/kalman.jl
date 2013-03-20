1fb81a4680f0e9c3935ef7846cdabb850e0f1f3e
Sam Urmy <sam@Thysanoessa>
2013-03-20 16:49:23 -0400

    Code for simulating SS models, plus Kalman filter

--- src/kalman.jl
+++ src/kalman.jl
@@ -13,7 +13,11 @@ type StateSpaceModel{T}
 	P0::Array{T}
 end
 
-function simulate_statespace(n, model, x0)
+function issquare(x::Array)
+	return size(x, 1) == size(x, 2)
+end
+
+function simulate_statespace(n, model)
 	# Generates a realization of a state space model.
 	#
 	# Arguments:
@@ -23,15 +27,56 @@ function simulate_statespace(n, model, x0)
 	#	Model defining the process
 	# x0 : Array{float}.
 	#	Inital state vector.
-	return None
+
+	# dimensions of the process and observation series
+	nx = length(model.x0)
+	ny = size(model.G, 1)
+	# create empty arrays to hold the state and observed series
+	x = zeros(nx, n)
+	x[:, 1] = model.x0
+	y = zeros(ny, n)
+	y[:, 1] = model.G * x[:, 1]
+	# Cholesky decompositions of the covariance matrices, for generating
+	# random noise
+	V_chol = chol(model.V)'
+	W_chol = chol(model.W)'
+	# Generate the series
+	for i=2:n
+		x[:, i] = model.F * x[:, i-1] + V_chol * rnorm(nx)
+		y[:, i] = model.G * x[:, i] + W_chol * rnorm(ny)
+	end
+	return x', y'
+end
+
+function check_dimensions(model)
+	@assert length(model.x0) == size(model.F, 2)
+	@assert size(model.F) == size(model.V)
+	@assert issquare(model.F)
+	@assert size(model.G, 2) == size(model.F, 1)
+	@assert size(model.G, 1) == size(model.W, 1)
+	@assert issquare(model.W)
+	@assert size(model.P0, 1) == length(model.x0)
+	@assert issquare(model.P0)
 end
 
 function KalmanFilter(y, model)
-	n = size(x, 1)
+	check_dimensions(model)
+	n = size(y, 1)
+	y = y'
 	x_est = zeros(length(model.x0), n)
-	x_est[1, :] = model.x0
+	x_est[:, 1] = model.F * model.x0
 	P0 = model.P0
-	for i=1:n
-		
+	I = eye(size(x_est, 1))
+	for i=2:n
+		# prediction
+		x_est[:, i] = model.F * x_est[:, i-1]
+		P1 = model.F * P0 * model.F' + model.V
+
+		innovation =  y[:, i] - model.G * x_est[:, i]
+		S = model.G * P1 * model.G' + model.W 	# Innovation covariance
+		K = P1 * model.G' * inv(S)				# Kalman gain
+		x_est[:, i] = x_est[:, i] + K * innovation
+		P0 = (I - K * model.G) * P1
 	end
+	return x_est'
 end
--- src/test_kalman.jl
+++ src/test_kalman.jl
@@ -1,9 +1,37 @@
+using Winston
+
 require("kalman.jl")
 
+srand(1)
+
+F = diagm([1.0, 1.0])
+V = diagm([2.0, 1.0])
+G = diagm([3.0 5.0])
+W = diagm([8.0, 2.5])
+x0 = rnorm(2)
+P0 = ones(2, 2) * 1e7
+mod1 = StateSpaceModel(F, V, G, W, x0, P0)
+
+check_dimensions(mod1)
 # Test that the thing can handle single- and multidimensional series
 
 # Test simulating 
+x, y = simulate_statespace(100, mod1)
+t = [1:size(x, 1)]
+
 
 # Test filtering
 
+x_est = KalmanFilter(y, mod1)
+
 # Test smoothing
+
+
+p = FramedPlot()
+add(p, Curve(t, x[:, 1], "color", "red"))
+add(p, Curve(t, x[:, 2], "color", "blue"))
+add(p, Curve(t, x_est[:, 1], "color", "red", "type", "dotted"))
+add(p, Curve(t, x_est[:, 2], "color", "blue", "type", "dotted"))
+file(p, "series.png")
+
+println("\n\nPassed all tests.")
