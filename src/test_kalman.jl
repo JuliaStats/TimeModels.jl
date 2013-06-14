150709d0bb432edb8a7ccf81b901993321920f9d
Sam Urmy <sam@Thysanoessa>
2013-06-13 22:58:24 -0400

    Add timing blocks to kalman test script

--- src/test_kalman.jl
+++ src/test_kalman.jl
@@ -2,13 +2,15 @@ using Winston
 
 require("kalman.jl")
 
+println("Finished loading packages.")
+
 srand(1)
 
 F = diagm([1.0])
 V = diagm([2.0])
 G = reshape([1, 2, -0.5], 3, 1)
 W = diagm([8.0, 2.5, 4.0])
-x0 = rnorm(1)
+x0 = randn(1)
 P0 = diagm([1e7])
 mod1 = StateSpaceModel(F, V, G, W, x0, P0)
 
@@ -16,19 +18,26 @@ check_dimensions(mod1)
 # Test that the thing can handle single- and multidimensional series
 
 # Test simulating 
-x, y = simulate_statespace(100, mod1)
-t = [1:size(x, 1)]
+println("Simulating series")
+@time begin
+	x, y = simulate_statespace(100, mod1)
+	t = [1:size(x, 1)]
+end
 
 
 # Test filtering
-
-filt = kalman_filter(y, mod1)
+println("Filtering")
+@time begin
+	filt = kalman_filter(y, mod1)
+	println("Log likelihood:")
+	println(filt.loglik)
+end
 
 # Test smoothing
+println("Smoothing")
+@time smooth = kalman_smooth(y, mod1)
 
-smooth = kalman_smooth(y, mod1)
-
-
+println("Plotting")
 p = FramedPlot()
 for i=1:3
 	add(p, Curve(t, y[:, i], "color", "grey"))
