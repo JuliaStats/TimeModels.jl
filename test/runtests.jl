51dd4f7c38bb91a2065b6c714cfc90242b76f25b
Theodore Papamarkou <theodore.papamarkou@gmail.com>
2014-05-15 22:46:14 +0100

    Started setting up FactTest testing

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,11 +1,25 @@
-my_tests = ["Kalman.jl"]
+# my_tests = ["Kalman.jl"]
 
-print_with_color(:cyan, "Running tests: ") 
-println("")
+# print_with_color(:cyan, "Running tests: ") 
+# println("")
 
-for my_test in my_tests
-    print_with_color(:magenta, "**   ") 
-    print_with_color(:blue, "$my_test") 
-    println("")
-    include(my_test)
+# for my_test in my_tests
+#     print_with_color(:magenta, "**   ") 
+#     print_with_color(:blue, "$my_test") 
+#     println("")
+#     include(my_test)
+# end
+
+module TimeModelTests
+
+using FactCheck #, MarketData
+
+tests = ["vanilla.jl"] #["Kalman.jl"]
+
+for test in tests
+  include(test)
+end
+
+exitstatus()
+
 end
---
+++ test/vanilla.jl
@@ -0,0 +1,3 @@
+facts("Checking that FactTest infrastructure works before running any tests") do
+  println("FactTest initiated")
+end
