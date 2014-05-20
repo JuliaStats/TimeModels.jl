1cb6e7d276e733ec7215af2d439924cbba76c7e0
milktrader <milktrader@gmail.com>
2014-05-20 10:56:56 -0400

    changed name of kalman to kalman_filter to track down capitalzation bug

--- test/all.jl
+++ test/all.jl
@@ -3,4 +3,4 @@
 include("arima.jl")
 include("diagnostic_tests.jl")
 include("garch.jl") 
-include("kalman.jl")
+include("kalman_filter.jl")
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,6 +3,6 @@ using FactCheck
 include("arima.jl")
 include("diagnostic_tests.jl")
 include("garch.jl") 
-include("kalman.jl")
+include("kalman_filter.jl")
 
 exitstatus()
