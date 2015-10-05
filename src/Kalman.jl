863f8d759b731883dece80a300e849ea61ed46b6
Robert Luke <codles@users.noreply.github.com>
2015-10-05 11:44:04 +0200

    Fix Union depecreciation
    WARNING: Union(args...) is deprecated, use Union{args...} instead.

--- src/Kalman.jl
+++ src/Kalman.jl
@@ -2,17 +2,17 @@ issquare(x::Matrix) = size(x, 1) == size(x, 2) ? true : false
 
 type StateSpaceModel{T}
 	# Process transition and noise covariance
-	F::Union(Matrix{T}, Matrix{Function}, Matrix{Any})
+	F::Union{Matrix{T}, Matrix{Function}, Matrix{Any}}
 	V::Matrix{T}
 	# Observation and noise covariance
-	G::Union(Matrix{T}, Matrix{Function}, Matrix{Any})
+	G::Union{Matrix{T}, Matrix{Function}, Matrix{Any}}
 	W::Matrix{T}
 	# Inital guesses at state and error covariance
 	x0::Vector{T}
 	P0::Matrix{T}
 
-	function StateSpaceModel(F::Union(Matrix{T}, Matrix{Function}, Matrix{Any}), V::Matrix{T},
-	                G::Union(Matrix{T}, Matrix{Function}, Matrix{Any}), W::Matrix{T}, x0::Vector{T}, P0::Matrix{T})
+	function StateSpaceModel(F::Union{Matrix{T}, Matrix{Function}, Matrix{Any}}, V::Matrix{T},
+	                G::Union{Matrix{T}, Matrix{Function}, Matrix{Any}}, W::Matrix{T}, x0::Vector{T}, P0::Matrix{T})
     @assert issquare(F)
     @assert size(F, 1) == length(x0)
     @assert issym(V)
@@ -29,8 +29,8 @@ type StateSpaceModel{T}
 	end
 end
 
-function StateSpaceModel{T <: Real}(F::Union(Matrix{T}, Matrix{Function}, Matrix{Any}), V::Matrix{T},
-                G::Union(Matrix{T}, Matrix{Function}, Matrix{Any}), W::Matrix{T}, x0::Vector{T}, P0::Matrix{T})
+function StateSpaceModel{T <: Real}(F::Union{Matrix{T}, Matrix{Function}, Matrix{Any}}, V::Matrix{T},
+                G::Union{Matrix{T}, Matrix{Function}, Matrix{Any}}, W::Matrix{T}, x0::Vector{T}, P0::Matrix{T})
 	StateSpaceModel{T}(F, V, G, W, x0, P0)
 end
 
