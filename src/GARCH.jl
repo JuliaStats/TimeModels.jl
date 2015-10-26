b621227ee65ce0e6dd773d14c3e90a3ef4200ef6
ShaoWei Teo <shaowei@macrolighthouse.com>
2015-10-26 21:02:49 +0800

    Standardize the code structure.
    Do only cosmetic changes.  There is rename of variables, removal of semicolon after statements, adding of the return statement, mainly things that make the code more Julia-like.

--- src/GARCH.jl
+++ src/GARCH.jl
@@ -5,16 +5,16 @@
 using NLopt, Distributions
 
 type GarchFit
-  data::Vector
-  params::Vector
-  llh::Float64
-  status::Symbol
-  converged::Bool
-  sigma::Vector
-  hessian::Array{Float64,2}
-  cvar::Array{Float64,2}
-  secoef::Vector
-  tval::Vector
+    data::Vector
+    params::Vector
+    llh::Float64
+    status::Symbol
+    converged::Bool
+    sigma::Vector
+    hessian::Array{Float64,2}
+    cvar::Array{Float64,2}
+    secoef::Vector
+    tval::Vector
 end
 
 function Base.show(io::IO ,fit::GarchFit)
@@ -23,7 +23,7 @@ function Base.show(io::IO ,fit::GarchFit)
     jbstat, jbp = jbtest(fit.data./fit.sigma)
 
     @printf io "Fitted garch model \n"
-    @printf io " * Coefficient(s):    %-15s%-15s%-15s\n" "omega" "alpha" "beta"
+    @printf io " * Coefficient(s):    %-15s%-15s%-15s\n" "α₀" "α₁" "β₁"
     @printf io "%-22s%-15.5g%-15.5g%-15.5g\n" "" fit.params[1] fit.params[2] fit.params[3]
     @printf io " * Log Likelihood: %.5g\n" fit.llh
     @printf io " * Converged: %s\n" fit.converged
@@ -33,33 +33,33 @@ function Base.show(io::IO ,fit::GarchFit)
     @printf io "   %-21s%-5s%-15.5g%-15.5g\n\n" "Jarque-Bera Test" "χ²" jbstat jbp
     @printf io " * Error Analysis:\n"
     @printf io "   %-7s%-15s%-15s%-15s%-15s\n" "" "Estimate" "Std.Error" "t value" "Pr(>|t|)"
-    @printf io "   %-7s%-15.5g%-15.5g%-15.5g%-15.5g\n" "omega" fit.params[1] fit.secoef[1] fit.tval[1] prt(fit.tval[1])
-    @printf io "   %-7s%-15.5g%-15.5g%-15.5g%-15.5g\n" "alpha" fit.params[2] fit.secoef[2] fit.tval[2] prt(fit.tval[2])
-    @printf io "   %-7s%-15.5g%-15.5g%-15.5g%-15.5g\n" "beta" fit.params[3] fit.secoef[3] fit.tval[3] prt(fit.tval[3])
+    @printf io "   %-7s%-15.5g%-15.5g%-15.5g%-15.5g\n" "α₀" fit.params[1] fit.secoef[1] fit.tval[1] prt(fit.tval[1])
+    @printf io "   %-7s%-15.5g%-15.5g%-15.5g%-15.5g\n" "α₁" fit.params[2] fit.secoef[2] fit.tval[2] prt(fit.tval[2])
+    @printf io "   %-7s%-15.5g%-15.5g%-15.5g%-15.5g\n" "β₁" fit.params[3] fit.secoef[3] fit.tval[3] prt(fit.tval[3])
 end
 
 function cdHessian(par, LLH)
-  eps = 1e-4 * par
-  n = length(par)
-  H = zeros(n,n)
-  for(i = 1:n)
-    for(j = 1:n)
-      x1 = copy(par)
-      x1[i] += eps[i]
-      x1[j] += eps[j]
-      x2 = copy(par)
-      x2[i] += eps[i]
-      x2[j] -= eps[j]
-      x3 = copy(par)
-      x3[i] -= eps[i]
-      x3[j] += eps[j]
-      x4 = copy(par)
-      x4[i] -= eps[i]
-      x4[j] -= eps[j]
-      H[i,j] = (LLH(x1)-LLH(x2)-LLH(x3)+LLH(x4)) / (4.*eps[i]*eps[j])
+    eps = 1e-4 * par
+    n = length(par)
+    H = zeros(n, n)
+    for i in 1:n
+        for j in 1:n
+            x₁ = copy(par)
+            x₁[i] += eps[i]
+            x₁[j] += eps[j]
+            x₂ = copy(par)
+            x₂[i] += eps[i]
+            x₂[j] -= eps[j]
+            x₃ = copy(par)
+            x₃[i] -= eps[i]
+            x₃[j] += eps[j]
+            x₄ = copy(par)
+            x₄[i] -= eps[i]
+            x₄[j] -= eps[j]
+            H[i,j] = (LLH(x₁) - LLH(x₂) - LLH(x₃) + LLH(x₄)) / (4 .* eps[i] * eps[j])
+        end
     end
-  end
-  H
+    return H
 end
 
 
@@ -67,53 +67,46 @@ function calculateVolatilityProcess(ɛ²::Vector, α₀, α₁, β₁)
     h = similar(ɛ²)
     h[1] = mean(ɛ²)
     for i = 2:length(ɛ²)
-        h[i] = α₀ + α₁ * ɛ²[i-1] + β₁ * h[i-1]
+        h[i] = α₀ + α₁ * ɛ²[i - 1] + β₁ * h[i - 1]
     end
     return h
 end
 
 
-function garchLLH(rets::Vector, x::Vector)
-  rets2   = rets.^2;
-  T = length(rets);
-  ht = zeros(T);
-  omega,alpha,beta = x;
-
-    ht = calculateVolatilityProcess(rets2, omega, alpha, beta)
-
-  -0.5*(T-1)*log(2*pi)-0.5*sum( log(ht) + (rets./sqrt(ht)).^2 );
+function garchLLH(y::Vector, x::Vector)
+    ɛ² = y .^ 2
+    T = length(y)
+    α₀, α₁, β₁ = x
+    h = calculateVolatilityProcess(ɛ², α₀, α₁, β₁)
+    return -0.5 * (T - 1) * log(2π) - 0.5 * sum(log(h) + (y ./ sqrt(h)) .^ 2)
 end
 
 function predict(fit::GarchFit)
- omega, alpha, beta = fit.params;
- rets = fit.data
- rets2   = rets.^2;
-
-    ht = calculateVolatilityProcess(rets2, omega, alpha, beta)
-
- sqrt(omega + alpha*rets2[end] + beta*ht[end]);
+    α₀, α₁, β₁ = fit.params
+    y = fit.data
+    ɛ² = y.^2
+    h = calculateVolatilityProcess(ɛ², α₀, α₁, β₁)
+    return sqrt(α₀ + α₁ * ɛ²[end] + β₁ * h[end])
 end
 
-function garchFit(rets::Vector)
-  rets2   = rets.^2;
-  T = length(rets);
-  ht = zeros(T);
-  function garchLike(x::Vector, grad::Vector)
-    omega,alpha,beta = x;
-
-        ht = calculateVolatilityProcess(rets2, omega, alpha, beta)
-
-    sum( log(ht) + (rets./sqrt(ht)).^2 );
-  end
-  opt = Opt(:LN_SBPLX, 3)
-  lower_bounds!(opt, [1e-10, 0.0, 0.0])
-  upper_bounds!(opt, [1; 0.3; 0.99])
-  min_objective!(opt, garchLike)
-  (minf,minx,ret) = NLopt.optimize(opt, [1e-5, 0.09, 0.89])
-  converged = minx[1]>0 && all(minx[2:3].>=0) && sum(minx[2:3])<1.0
-  H = cdHessian(minx, x->garchLLH(rets, x))
-  cvar = -inv(H)
-  secoef = sqrt(diag(cvar))
-  tval = minx./secoef
-  out = GarchFit(rets, minx, -0.5*(T-1)*log(2*pi)-0.5*minf, ret, converged, sqrt(ht), H, cvar, secoef, tval)
+function garchFit(y::Vector)
+    ɛ² = y.^2
+    T = length(y)
+    h = zeros(T)
+    function garchLike(x::Vector, grad::Vector)
+        α₀, α₁, β₁ = x
+        h = calculateVolatilityProcess(ɛ², α₀, α₁, β₁)
+        sum(log(h) + (y ./ sqrt(h)) .^ 2)
+    end
+    opt = Opt(:LN_SBPLX, 3)
+    lower_bounds!(opt, [1e-10, 0.0, 0.0])
+    upper_bounds!(opt, [1, 0.3, 0.99])
+    min_objective!(opt, garchLike)
+    (minf, minx, ret) = NLopt.optimize(opt, [1e-5, 0.09, 0.89])
+    converged = minx[1] > 0 && all(minx[2:3] .>= 0) && sum(minx[2:3]) < 1.0
+    H = cdHessian(minx, x -> garchLLH(y, x))
+    cvar = -inv(H)
+    secoef = sqrt(diag(cvar))
+    tval = minx ./ secoef
+    return GarchFit(y, minx, -0.5 * (T - 1) * log(2π) - 0.5 * minf, ret, converged, sqrt(h), H, cvar, secoef, tval)
 end
