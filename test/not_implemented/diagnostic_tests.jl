d1830bc4e082b87a9cff6d0f0a4716ef1bb251be
Andreas Noack <andreasnoackjensen@gmail.com>
2017-10-20 16:43:17 +0200

    Switch from FactCheck to testsets
    Fix some automatic broadcast deprecations

--- test/not_implemented/diagnostic_tests.jl
+++
@@ -1,9 +0,0 @@
-using MarketData
-
-facts("diagnostic tests produce accurate results") do
-
-  context("jaques-berra test") do
-      @fact length(cl.values) => 500
-  end
-end
-
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,7 @@
 # adding using TimeModels to this file is odd, but it's a trick to get travis to pass
 
-using FactCheck, TimeModels
+using Base.Test
+using TimeModels
 
 include("kalman_filter.jl")
 include("parameter_estimation.jl")
-
-exitstatus()
